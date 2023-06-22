4de05d5c7977aceb98482068744a8e139f104d94
Sav S. Mach <sav@loophole.cc>
2023-06-22 07:10:02 -0300

    Add: algo/warmup-diagonal-distances.
    Signed-off-by: Sav S. Mach <sav@loophole.cc>

--- algo/warmup-diagonal-difference/src/main.rs
+++ algo/warmup-diagonal-difference/src/main.rs
@@ -1,6 +1,4 @@
-use std::env;
-use std::fs::File;
-use std::io::{self, BufRead, Write};
+use std::io::{self, BufRead};
 
 /*
  * Complete the 'diagonalDifference' function below.
@@ -9,29 +7,47 @@ use std::io::{self, BufRead, Write};
  * The function accepts 2D_INTEGER_ARRAY arr as parameter.
  */
 
-fn diagonal_difference(arr: &[Vec<i32>]) -> i32 {
-    return 0
+fn diagonal_difference(matrix: &[Vec<i32>]) -> i32 {
+    let rows: usize = matrix.len();
+    let mut row: usize = 0;
+    let mut primary: i32 = 0;
+    let mut secondary: i32 = 0;
+    while row < rows {
+        let cols: &Vec<i32> = &matrix[row];
+        assert_eq!(cols.len(), rows); // square matrix
+        primary += cols[row];
+        secondary += cols[rows - row - 1];
+        row += 1;
+    }
+    return (primary - secondary).abs();
 }
 
 fn main() {
     let stdin = io::stdin();
     let mut stdin_iterator = stdin.lock().lines();
 
-    let n = stdin_iterator.next().unwrap().unwrap().trim().parse::<i32>().unwrap();
+    let n = stdin_iterator
+        .next()
+        .unwrap()
+        .unwrap()
+        .trim()
+        .parse::<i32>()
+        .unwrap();
 
     let mut arr: Vec<Vec<i32>> = Vec::with_capacity(n as usize);
 
     for i in 0..n as usize {
         arr.push(Vec::with_capacity(n as usize));
 
-        arr[i] = stdin_iterator.next().unwrap().unwrap()
+        arr[i] = stdin_iterator
+            .next()
+            .unwrap()
+            .unwrap()
             .trim_end()
             .split(' ')
             .map(|s| s.to_string().parse::<i32>().unwrap())
             .collect();
     }
 
-    let result = diagonal_difference(&arr);
-
-    println!("{}", result).ok();
+    println!("{}", diagonal_difference(&arr));
 }
